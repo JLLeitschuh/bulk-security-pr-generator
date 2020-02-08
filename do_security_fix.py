9beeb894219a6a91e07b305b95e27aa6f95ea54d
Jonathan Leitschuh <Jonathan.Leitschuh@gmail.com>
2020-02-08 17:32:12 -0500

    Implement regex based fixer

--- do_security_fix.py
+++ do_security_fix.py
@@ -2,6 +2,8 @@
 
 import os
 import json
+import re
+import logging
 import shutil
 import subprocess
 from collections import Counter
@@ -11,7 +13,16 @@ from typing import Generator, List, Dict
 clone_repos_location = 'cloned_repos'
 
 # Cleanup method to get rid of previous files
-shutil.rmtree(clone_repos_location)
+if os.path.isdir(clone_repos_location):
+    shutil.rmtree(clone_repos_location)
+os.mkdir(clone_repos_location)
+
+p_fix_regex = \
+    re.compile(
+        r'(?<=repository>)((?:(?!repository>).)*)(<url>\s*)http://(\S*)(\s*</url>)',
+        re.IGNORECASE + re.MULTILINE + re.DOTALL
+    )
+replacement = r'\1\2https://\3\4'
 
 
 def subprocess_run(args: List[str], cwd: str):
@@ -24,7 +35,7 @@ class VulnerableProjectFiles:
     files: Dict[str, int]
 
     def project_file_name(self) -> str:
-        return self.project_name.split('/')[1]
+        return clone_repos_location + '/' + self.project_name.split('/')[1]
 
     def print(self):
         print(self.project_name)
@@ -34,8 +45,25 @@ class VulnerableProjectFiles:
     def do_clone(self):
         subprocess_run(['hub', 'clone', self.project_name], cwd=clone_repos_location)
 
-    def run_in(self, args: List[str]):
-        subprocess_run(args, cwd=clone_repos_location + '/' + self.project_file_name())
+    def do_run_in(self, args: List[str]):
+        subprocess_run(args, cwd=self.project_file_name())
+
+    def do_fix_vulnerable_file(self, file: str, expected_fix_count: int):
+        file_being_fixed: str = self.project_file_name() + file
+        with open(file_being_fixed) as vulnerableFile:
+            contents: str = vulnerableFile.read()
+
+        new_contents, count = p_fix_regex.subn(replacement, contents)
+        if count != expected_fix_count:
+            logging.warning('Fix did match expected fix count: (expected: %d, actual: %d)', expected_fix_count, count)
+
+        with open(file_being_fixed, 'w') as vulnerableFile:
+            vulnerableFile.write(new_contents)
+
+    def do_fix_vulnerability(self):
+        for file in self.files:
+            self.do_fix_vulnerable_file(file, self.files[file])
+        self.do_run_in(['git', 'diff'])
 
 
 def list_all_json_files() -> Generator[str, None, None]:
@@ -50,14 +78,15 @@ def list_all_json_files() -> Generator[str, None, None]:
 def read_repository_and_file_names(json_file_name: str) -> VulnerableProjectFiles:
     with open(json_file_name) as jsonFile:
         data = json.load(jsonFile)
-        project_name: str = data['project']['name']
-        files = Counter([obj[0]['file'] for obj in data['data']])
-        return VulnerableProjectFiles(project_name, files)
+    project_name: str = data['project']['name']
+    files = Counter([obj[0]['file'] for obj in data['data']])
+    return VulnerableProjectFiles(project_name, files)
 
 
 def process_vulnerable_project(project: VulnerableProjectFiles):
     project.print()
     project.do_clone()
+    project.do_fix_vulnerability()
     pass
 
 
@@ -66,6 +95,9 @@ for json_file in list_all_json_files():
     vulnerable = read_repository_and_file_names(json_file)
     vulnerable.print()
     if 'jlleitschuh' in vulnerable.project_name.lower():
+        vulnerable_projects.append(vulnerable)
+
+    if 'epos' in vulnerable.project_name.lower():
         vulnerable_projects.append(vulnerable)
 
 print()
