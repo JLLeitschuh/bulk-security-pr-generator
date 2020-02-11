8368b44506fab6b0c1958a623251dcaba01d721c
Jonathan Leitschuh <Jonathan.Leitschuh@gmail.com>
2020-02-11 10:36:37 -0500

    Finish save state for o-r

--- do_security_fix.py
+++ do_security_fix.py
@@ -359,6 +359,7 @@ async def do_run_everything():
         if vulnerable.project_name == 'apache/servicemix4-bundles' or \
                 vulnerable.project_name == 'marcust/struts1' or \
                 vulnerable.project_name == 'qoomon/aws-sqs' or \
+                vulnerable.project_name == 'pocariP/project' or \
                 vulnerable.project_name == 'meertensinstituut/mtas' or \
                 '/maven' in vulnerable.project_name:
             # TODO: Come back to this black listed project later
